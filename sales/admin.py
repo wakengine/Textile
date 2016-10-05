198d43dd5521a518c998347097f060f9b6eb8d75
Eric Liu <wakengine@gmail.com>
2016-10-05 19:41:10 +0800

    Add more fields

--- sales/admin.py
+++ sales/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Shop, SalesList, SalesDetail, Cloth, Ownership
+from .models import Shop, Cloth, Image, SalesList, SalesDetail, Ownership
 
 admin.site.register(Shop)
 admin.site.register(Cloth)
+admin.site.register(Image)
 admin.site.register(SalesList)
 admin.site.register(SalesDetail)
 admin.site.register(Ownership)
--- sales/models.py
+++ sales/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
+class Image(models.Model):
+    url = models.CharField(max_length=100)
+    file_name = models.CharField(max_length=100)
+    file = models.FileField()
+    description = models.CharField(max_length=100)
+    uploaded_time = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.file_name
+
+
 class Shop(models.Model):
     owner_name = models.CharField(max_length=20)
     shop_name = models.CharField(max_length=20)
@@ -32,7 +44,6 @@ class Cloth(models.Model):
     ref_price = models.FloatField()
     is_per_meter = models.BooleanField(default=True)
     used_for = models.CharField(max_length=500)
-    image_url = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
     created_time = models.DateTimeField(auto_now_add=True)
     timestamp = models.DateTimeField(auto_now=True)
@@ -46,26 +57,30 @@ class Cloth(models.Model):
 
 class Ownership(models.Model):
     shop = models.ForeignKey(Shop)
-    goods = models.ForeignKey(Cloth)
+    cloth = models.ForeignKey(Cloth)
+    price = models.FloatField()
+    image = models.ForeignKey(Image)
+    description = models.TextField(max_length=1000)
     created_time = models.DateTimeField(auto_now_add=True)
     timestamp = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.goods.name + '@' + self.shop.owner_name
+        return self.cloth.name + '@' + self.shop.owner_name
 
 
 class SalesList(models.Model):
     serial_no = models.CharField(max_length=20)
     customer = models.ForeignKey(Shop)
-    goods = models.ForeignKey(Cloth)
+    cloth = models.ForeignKey(Cloth)
     color = models.CharField(max_length=20)
     price_per_unit = models.FloatField()
     total_units = models.FloatField()
     total_bundles = models.FloatField()
     total_price = models.FloatField()
-    already_paid = models.FloatField()
+    total_paid = models.FloatField()
     is_withdrawn = models.BooleanField(default=False)
     is_warehouse = models.BooleanField(default=False)  # Need to be set to True for WareHouse users
+    image = models.ForeignKey(Image)
     order_date = models.DateTimeField(auto_now=False, auto_now_add=False, auto_created=False)
     timestamp = models.DateTimeField(auto_now=True)
 
